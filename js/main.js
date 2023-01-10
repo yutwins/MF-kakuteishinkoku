cd4f2331dedb957d5d33f346557141de80a25a92
Yuto Nakamura <yutwo0717@gmail.com>
2023-01-10 09:37:05 +0900

    [fix]

--- js/main.js
+++ js/main.js
@@ -29,3 +29,31 @@ for (let i = 0; i < qaQ.length; i++) {
         this.parentNode.classList.toggle('is-active');
     });
 }
+
+const radio1 = document.querySelector('.simulation-a__input01');
+const radio2 = document.querySelector('.simulation-a__input02');
+const radio3 = document.querySelector('.simulation-a__input03');
+const radio4 = document.querySelector('.simulation-a__input04');
+function showItems() {
+    if (radio1.checked && radio3.checked) {
+        // 「お得に〜〜」と「はい」を選択した場合
+        document.querySelector('.simulation-result__container01').classList.add('is-selected');
+        document.querySelector('.simulation-result__container02').classList.remove('is-selected');
+        document.querySelector('.simulation-result__container03').classList.remove('is-selected');
+    } else if (radio1.checked && radio4.checked) {
+        // 「お得に〜〜」と「いいえ」を選択した場合
+        document.querySelector('.simulation-result__container01').classList.remove('is-selected');
+        document.querySelector('.simulation-result__container02').classList.add('is-selected');
+        document.querySelector('.simulation-result__container03').classList.remove('is-selected');
+    } else if (radio2.checked && radio3.checked) {
+        // 「所得税だけではなく〜〜」と「はい」を選択した場合
+        document.querySelector('.simulation-result__container01').classList.remove('is-selected');
+        document.querySelector('.simulation-result__container02').classList.remove('is-selected');
+        document.querySelector('.simulation-result__container03').classList.add('is-selected');
+    } else if (radio2.checked && radio4.checked) {
+        // 「所得税だけではなく〜〜」と「いいえ」を選択した場合
+        document.querySelector('.simulation-result__container01').classList.add('is-selected');
+        document.querySelector('.simulation-result__container02').classList.remove('is-selected');
+        document.querySelector('.simulation-result__container03').classList.remove('is-selected');
+    }
+}
